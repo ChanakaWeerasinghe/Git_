262973dcbd90e7bf1c38b8a29b8e8d60dc064739
Chanaka Weerasinghe <chanaka.weerasinghe@interblocks.com>
2019-08-23 17:47:40 +0530

    fdata

--- src/newa.java
+++ src/newa.java
@@ -2,6 +2,13 @@ public class newa {
     public static void main(String[] args) {
         System.out.println("this is the merging");
         System.out.println("master Branch ");
+        System.out.println("this is the merging");
+        System.out.println("master Branch ");
+        System.out.println("this is the merging");
+        System.out.println("master Branch ");
+        System.out.println("this is the merging");
+        System.out.println("master Branch ");
+
     }
 }
 
