1177fea603d50100e3bf318cd5f41d2c14383cad
Chanaka Weerasinghe <chanaka.weerasinghe@interblocks.com>
2019-08-23 17:38:29 +0530

    git config --global push.followTags true

--- src/newa.java
+++ src/newa.java
@@ -2,7 +2,7 @@ public class newa {
     public static void main(String[] args) {
         System.out.println("this is the merging");
         System.out.println("master Branch ");
-        System.out.println("git push --follow-tags");
+        System.out.println("git config --global push.followTags true");
     }
 }
 
