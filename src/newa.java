54c57dd468967789aada45e6b2907f7eb471ac1b
Chanaka Weerasinghe <chanaka.weerasinghe@interblocks.com>
2019-08-23 17:35:02 +0530

    tag

--- src/newa.java
+++ src/newa.java
@@ -2,6 +2,7 @@ public class newa {
     public static void main(String[] args) {
         System.out.println("this is the merging");
         System.out.println("master Branch ");
+        System.out.println("add new tag");
     }
 }
 
